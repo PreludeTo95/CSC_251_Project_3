b428d5f5fe8aab9b05b39e6894ad3782704df500
PreludeTo95 <71227983+PreludeTo95@users.noreply.github.com>
2022-09-12 18:28:54 -0400

    Added code for Policy class

---
+++ Policy.java
@@ -0,0 +1,148 @@
+public class Policy
+{
+   private String policyNumber;
+   private String providerName;
+   private String firstName;
+   private String lastName;
+   private int age;
+   private boolean smoker;
+   private double height;
+   private double weight;
+   
+   //no-arg constructor
+   public Policy()
+   {
+      //policyNumber is a string because it should not be used in calculations
+      policyNumber = "0000";
+      providerName = "Unassigned";
+      firstName = "John";
+      lastName = "Doe";
+      age = 99;
+      smoker = false;
+      height = 70;
+      weight = 200;
+   }
+   
+   //arg constructor (pirate constructor?)
+   public Policy(String policyNum, String provider, String fName, String lName, int howOld, boolean smokerStatus, double howTall, double howHeavy)
+   {
+      policyNumber = policyNum;
+      providerName = provider;
+      firstName = fName;
+      lastName = lName;
+      age = howOld;
+      smoker = smokerStatus;
+      height = howTall;
+      weight = howHeavy;
+   }
+   
+   //getter and setter methods for every attribute
+   public void setPolicyNumber(String p)
+   {
+      policyNumber = p;
+   }
+   
+   public String getPolicyNumber()
+   {
+      return policyNumber;
+   }
+   
+   public void setProviderName(String p)
+   {
+      providerName = p;
+   }
+   
+   public String getProviderName()
+   {
+      return providerName;
+   }
+   
+   public void setFirstName(String f)
+   {
+      firstName = f;
+   }
+   
+   public String getFirstName()
+   {
+      return firstName;
+   }
+   
+   public void setLastName(String l)
+   {
+      lastName = l;
+   }
+   
+   public String getLastName()
+   {
+      return lastName;
+   }
+   
+   public void setAge(int a)
+   {
+      age = a;
+   }
+   
+   public int getAge()
+   {
+      return age;
+   }
+   
+   public void setSmoker(boolean s)
+   {
+      smoker = s;
+   }
+   
+   public boolean getSmoker()
+   {
+      return smoker;
+   }
+   
+   public void setHeight(double h)
+   {
+      height = h;
+   }
+   
+   public double getHeight()
+   {
+      return height;
+   }
+   
+   public void setWeight(double w)
+   {
+      weight = w;
+   }
+   
+   public double getWeight()
+   {
+      return weight;
+   }
+   
+   public double calculateBMI()
+   {
+      return ((weight * 703) / (height * height));
+   }
+   
+   public double calculatePolicyPrice()
+   {
+      double policyPrice = 600;
+      double BMI = calculateBMI();
+      //declaring BMI as a variable saves 1 method call
+      
+      if (age > 50)
+      {
+         policyPrice += 75;
+      }
+      
+      if (smoker)
+      {
+         policyPrice += 100;
+      }
+      
+      if (BMI > 35)
+      {
+         policyPrice += ((BMI - 35) * 20);
+      }
+      
+      return policyPrice;
+   }
+}
---
+++ PolicyDemo.java
@@ -0,0 +1,7 @@
+public class PolicyDemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
+    }
+}
